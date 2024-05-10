8aa84143d52c024d71123cf75c01287ad74263b5
AppleCrumbleTea <1020593@student.sae.edu.au>
2024-05-10 20:01:38 +1000

    pls program particles

---
+++ Assets/DayNightController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightController : MonoBehaviour
+{
+    public GameObject targetLight;
+    public GameObject targetMainCamera;
+    public Material[] skys;
+    public float dayTimer;
+    public bool isCycle;
+
+    private void Awake()
+    {
+        targetLight = GameObject.FindGameObjectWithTag("Light");
+        targetMainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+
+    void Start()
+    {
+        dayTimer = targetLight.GetComponent<Light>().intensity;
+    }
+ 
+    void Update()
+    {
+        if (!isCycle)
+        {
+            targetLight.GetComponent<Light>().intensity = dayTimer -= Time.deltaTime * 0.3f;
+            if(dayTimer <= 0)
+            {
+                isCycle = true;
+            }
+        }
+
+            else if(isCycle)
+
+        {
+            targetLight.GetComponent<Light>().intensity = dayTimer += Time.deltaTime * 0.3f;
+
+            if (dayTimer <= 1)
+            {
+                isCycle = false;
+            }
+        }
+
+        ChangeCycle();
+    }
+
+
+    void ChangeCycle()
+    {
+        if(dayTimer >= 0.9f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[0];
+        }
+    }
+}
