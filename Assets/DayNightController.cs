a22d9da49ab31ff9ec69ffa77027dcb561188115
MarcusGeorgiou <1027503@student.sae.edu.au>
2024-05-10 21:10:33 +1000

    skyboseS (plural)

--- Assets/DayNightController.cs
+++ Assets/DayNightController.cs
@@ -9,6 +9,8 @@ public class DayNightController : MonoBehaviour
     public Material[] skys;
     public float dayTimer;
     public bool isCycle;
+
+    public float timeScale;
     
     void Start()
     {
@@ -19,19 +21,17 @@ public class DayNightController : MonoBehaviour
     {
         if (!isCycle)
         {
-            targetLight.intensity = dayTimer -= Time.deltaTime * 0.3f;
+            targetLight.intensity = dayTimer -= Time.deltaTime * timeScale;
             if(dayTimer <= 0)
             {
                 isCycle = true;
             }
         }
-
-            else if(isCycle)
-
+        else if(isCycle)
         {
-            targetLight.intensity = dayTimer += Time.deltaTime * 0.3f;
+            targetLight.intensity = dayTimer += Time.deltaTime * timeScale;
 
-            if (dayTimer >= 1)
+            if (dayTimer >= 1.5f)
             {
                 isCycle = false;
             }
@@ -43,9 +43,29 @@ public class DayNightController : MonoBehaviour
 
     void ChangeCycle()
     {
-        if(dayTimer >= 0.9f)
+        if(dayTimer >= 0.125f)
         {
             targetMainCamera.GetComponent<Skybox>().material = skys[0];
+        }
+        else if (dayTimer >= 0.1f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[1];
+        }
+        else if (dayTimer >= 0.75f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[2];
+        }
+        else if (dayTimer >= 0.5f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[3];
+        }
+        else if (dayTimer >= 0.25f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[4];
+        }
+        else if (dayTimer >= 0f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[5];
         }
     }
 }
